b707c6b1fba9ee03a7028cd1d189f3b835000aa9
Sumit More <gusunil78@gmail.com>
2023-03-13 11:52:47 +0530

    main.java file was created

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        int i=1;
+        System.out.println("%d %d %d"+ i++ + --i + ++i);
+        Scanner input = new Scanner(System.in);
+            System.out.println(input.nextLine());
+
+        System.out.println(input.nextLine());
+        float marks = 45.67f;
+        int rollno = input.nextInt();
+        System.out.println("Your roll number is "+ rollno);
+    }
+}
